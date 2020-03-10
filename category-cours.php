99d5825094b276f0408ba482f81ee15a93c1f1f4
Jeremie Bouchard <jeremieb4360@gmail.com>
2020-03-10 16:18:30 -0400

    fin troisieme question

--- category-cours.php
+++ category-cours.php
@@ -12,24 +12,32 @@ get_header();
         the_post_thumbnail('full');
    
         
+ 
+    
+    $query1 = new WP_Query( $args );
 
          endwhile; // End of the loop.
  
 
     $args = array(
-        "category_name" => "cours",
-        "posts_per_page" => 30,
-        "order"=>"asc",
-        "orderby" => 'title'
+            "category_name" => "cours",
+            "posts_per_page" => 30 ,
+            "order"=>"ASC",
+            "orderby" => 'title'
         
     );
     $query1 = new WP_Query( $args );
 
 // The Loop
 while ( $query1->have_posts() ) {
+    $oSession = substr(get_the_title(),4,1);
+    $oDomaine = substr(get_the_title(),5,1);
+    echo '<div style="background-color:red; ">';
+    echo '<a class="oCours" href='.get_permalink().'>' . get_the_title() .  '<p style="display:inline;color:green"> - 
+    Session :'.$oSession.'</p><p style="display:inline;color:blue"> - Domaine : '.$oDomaine. '</p></a>';
 echo '<br>'; 
 $query1->the_post();
- echo '<h3>' . get_the_title() . '</h3>';
+ //echo '<h3>' . get_the_title() . '</h3>';
  echo '<p>'.get_the_excerpt(). '</p>';
 }
 
--- functions.php
+++ functions.php
@@ -3,15 +3,15 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
-}/*
-add_action( 'pre_get_posts', 'extraire_cours' );
-function extraire_cours( $query ) {
-    if ($query->is_category('evenement'))
-    {
-       $query->set( 'posts_per_page', 3 );
-       $query->set( 'orderby', 'date' );
-       $query->set( 'order', 'ASC' );
-    }
- }
-*/
+}
+// add_action( 'pre_get_posts', 'extraire_cours' );
+// function extraire_cours( $query ) {
+//     if ($query->is_category('cours'))
+//     {
+//        $query->set( 'posts_per_page', 30 );
+//        $query->set( 'orderby', 'title' );
+//        $query->set( 'order', 'ASC' );
+//     }
+//  }
+
 // show_admin_bar( true );
